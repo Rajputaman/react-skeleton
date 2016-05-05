676db59d841a7bbb29d101950dc87751d2ced868
Aman Rajput <Aman Rajput>
2016-05-05 13:31:38 +0530

    react-skeleton

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -1,32 +1,66 @@
   var React = require('react');
   var List = require('./List.jsx');
   var ListManager = React.createClass({
+        //Called once in Component life cycle - an initializer
         getInitialState: function(){
           return {items: [], newItemText:''};
         },
         onChange: function(e){
+          //Update the state everytime a keystroke is typed
           this.setState({newItemText: e.target.value});
         },
         handleSubmit: function(e){
+          //Stop the button from getting clicks since we r using form onSubmit
           e.preventDefault();
 
+          //Grab the current list of items
           var currentItems = this.state.items;
 
+            // Add the new items to the list
             currentItems.push(this.state.newItemText);
 
+            //Update the main item list with the new list  and clear newItemText
             this.setState({items: currentItems, newItemText:''});
         },
 
         render: function(){
+          //onChange is called with every keystroke so we can store the most recent data entered
+          //value is what the users sees in the input box - we point this to newItemText  so its update on every
+
+          var divStyle = {
+            marginTop: 10
+          }
+
+          var headingStyle ={
+
+          }
+
+          if (this.props.headingColor){
+            headingStyle.background = this.props.headingColor;
+          }
+
           return (
-            <div>
-                <h3>{this.props.title}</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.onChange} value={this.state.newItemText} />
-                    <button>Add</button>
-                </form>
-                <List items={this.state.items} />
-            </div>
+            <div style={divStyle} className="col-sm-4">
+              <div className="panel panel-primary">
+                  <div style={headingStyle} className="panel-heading">
+                    <h3>{this.props.title}</h3>
+                  </div>
+                  <div className="row panel-body">
+                    <form onSubmit={this.handleSubmit}>
+                      <div className="col-sm-9">
+                        <input className="form-control" onChange={this.onChange} value={this.state.newItemText} />
+                      </div>
+                      <div className="col-sm-2">
+                        <button className="btn btn-primary">Add</button>
+                      </div>
+                    </form>
+                  </div>
+
+                  <List items={this.state.items} />
+              </div>
+          </div>
           );
         }
   });
+
+module.exports = ListManager;
